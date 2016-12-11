8c6624fbaaadcb34401fe43445ca704203ed98d6
Luigui <luisguilhermepdea@gmail.com>
2016-12-11 20:05:43 -0200

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>learning</title>
+  </head>
+  <body>
+    <p>defining variable</p>
+    <?php include 'var.php';?>
+  </body>
+</html>
---
+++ var.php
@@ -0,0 +1,4 @@
+<?php
+  $var = 12;
+  echo "var = $var";
+?>
